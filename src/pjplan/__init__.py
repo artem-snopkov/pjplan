57583abc6b13ca7e3f3e5f890374bf301ff201b2
artem <artem.snopkov@gmail.com>
2023-03-22 08:48:31 +0700

    Initial

---
+++ src/pjplan/__init__.py
@@ -0,0 +1,11 @@
+"""
+Пакет содержит API для работы с проектами и расписаниями задач.
+"""
+from pjplan.wbs import Task, WBS
+from pjplan.calendar import IWorkCalendar, GenericCalendar, CapacityCalendar, DEFAULT_CALENDAR
+from pjplan.resource import IResource, Resource, DEFAULT_RESOURCE
+from pjplan.schedule import DefaultScheduler, Sprint
+from pjplan.io import TaskRaw
+from pjplan.io.df_io import to_df
+from pjplan.io.csv_io import read_csv, write_csv
+from pjplan.viz.tasksheet import task_sheet
