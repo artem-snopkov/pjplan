ec9773dc6ced58d0828caf145c4af2a25976fcde
artem <artem.snopkov@gmail.com>
2023-04-15 15:03:23 +0700

    Documentation

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'pjplan'
+copyright = '2023, Artem Snopkov'
+author = 'Artem Snopkov'
+release = '0.0.8'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser', 'nbsphinx']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'en'
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+.. pjplan documentation master file, created by
+   sphinx-quickstart on Sat Apr 15 13:01:36 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+
+
+pjplan: Python library for project planning and analysis
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   ru/index
+
